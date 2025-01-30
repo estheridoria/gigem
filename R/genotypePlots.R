392304c7d879208f9d1b3af1091cecb4425ef2f6
Edoria <estheridoria@gmail.com>
2025-01-30 15:23:35 -0600

    Fix genotypePlots
    output p_values csv & make the astrick show up.

--- R/genotypePlots.R
+++ R/genotypePlots.R
@@ -24,20 +24,16 @@
 #' @keywords internal
 
 genotypePlots <- function(dt_curated_final, summary_dt_final, font) {
-
-  #   # Get the all unique combinations of Parameters
-  #   condition_combinationss <- data.table::data.table(unique(dt_finalSummary[, .(Sex, Genotype, Temperature,
-  #                                                       Treatment, Environment, Light)]))
-  columns_to_consider <- c("Sex", "Genotype", "Temperature", "Treatment", "Environment", "Light")
-
   #Dynamically exclude columns where the value is equal to divisions[1] &
-  condition_combinations <- unique(dt_finalSummary[,
-                                                   .SD,
-                                                   .SDcols = columns_to_consider[columns_to_consider != divisions[1]]
-  ])
+  columns_to_consider <- c("Sex", "Genotype", "Temperature", "Treatment", "Environment", "Light")
+  condition_combinations <- unique(summary_dt_final[,.SD,.SDcols = columns_to_consider[columns_to_consider != divisions[1]]])
+
+  u <- length(unique(summary_dt_final[[divisions[1]]])) # Dynamic width adjustment of combined plot
+  p_values <- data.table::data.table()
+  # p1title <- list()
 
   # Function to create sleep duration plots
-  create_sleeptime_plot <- function(plot_data, yParam, Yname, limits, geom, font) {
+  create_sleeptime_plot <- function(plot_data, yParam, Yname, limits, geom, font, p_value = NULL) {
     pointplot<- ggplot2::ggplot(plot_data, ggplot2::aes(x = .data[[divisions[1]]], y = .data[[yParam]]))
     if(geom == "bar"){
       pointplot <- pointplot +
@@ -63,72 +59,42 @@ genotypePlots <- function(dt_curated_final, summary_dt_final, font) {
                      axis.text.x = ggplot2::element_text(size = 16, angle = 45, vjust = 1, hjust= 1),
                      axis.text.y = ggplot2::element_text(size = 16),
                      legend.position = "none")
+    if (!is.null(p_value)) {
+      # Define thresholds and corresponding labels
+      thresholds <- c(0.0001, 0.001, 0.01, 0.05)
+      labels <- c("****", "***", "**", "*")
 
-# Perform t-test if the 'Treatment' column has both 'Grp' and 'Iso'
-if (any(plot_data[,Treatment] == "Grp") & any(plot_data[,Treatment ] == "Iso")){
+      # Find the corresponding label directly using logical comparisons
+      p_label <- ifelse(p_value <= thresholds[1], labels[1],
+                 ifelse(p_value <= thresholds[2], labels[2],
+                 ifelse(p_value <= thresholds[3], labels[3],
+                 ifelse(p_value <= thresholds[4], labels[4], ""))))
 
-  # Perform a t-test
-  t_test_result <- t.test(plot_data[Treatment == "Grp", get(yParam)],plot_data[Treatment == "Iso", get(yParam)])
-
-  # Get the p-value from the t-test
-  p_value <- t_test_result$p.value
-
-  # Add the p-value symbols as an annotation above the bars
-  if(p_value <= 0.05 & p_value >0.01){
-  pointplot <- pointplot + ggplot2::annotate("text", x = 1.5, y = limits,
-               label = paste("*"), size = 5, color = "black")+
-    ggplot2::geom_segment(ggplot2::aes(x = 1, xend = 2, y = limits-(limits/20), yend = limits-(limits/20)),
-                          color = "black", linewidth = 1)}
-  if(p_value <= 0.01 & p_value >0.001){
-    pointplot <- pointplot + ggplot2::annotate("text", x = 1.5, y = limits,
-                                               label = paste("**"), size = 5, color = "black")+
-      ggplot2::geom_segment(ggplot2::aes(x = 1, xend = 2, y = limits-(limits/20), yend = limits-(limits/20)),
-                            color = "black", linewidth = 1)}
-  if(p_value <= 0.001 & p_value >0.0001){
-    pointplot <- pointplot + ggplot2::annotate("text", x = 1.5, y = limits,
-                                               label = paste("***"), size = 5, color = "black")+
-      ggplot2::geom_segment(ggplot2::aes(x = 1, xend = 2, y = limits-(limits/20), yend = limits-(limits/20)),
-                            color = "black", linewidth = 1)}
-  if(p_value <= 0.0001){
-    pointplot <- pointplot + ggplot2::annotate("text", x = 1.5, y = limits,
-                                               label = paste("****"), size = 5, color = "black")+
-      ggplot2::geom_segment(ggplot2::aes(x = 1, xend = 2, y = limits-(limits/20), yend = limits-(limits/20)),
-                            color = "black", linewidth = 1)}
-  if(p_value < 0.05){
-    # Add the p-value as an annotation above the bars
-    pointplot <- pointplot +
-      ggplot2::annotate("text", x = 1.5, y = limits-2*(limits/20),
-                        label = paste("p =", round(p_value, 4)), size = 5, color = "black")
-  }
-}
+      # If a label is assigned, annotate the plot
+      if (p_label !="") {
+        pointplot <- pointplot +
+          ggplot2::annotate("text", x = 1.5, y = limits,
+                            label = p_label, size = 5, color = "black") +
+          ggplot2::geom_segment( mapping = NULL, x = 1, xend = 2, y = (limits - (limits / 20)), yend = (limits - (limits / 20)),
+                                color = "black", linewidth = 1)
+      }
+      } else {
+        # Handle case when p_value is NULL or empty
+        warning("p_value is NULL or empty, no annotation added.")
+    }
     return(pointplot)
   }
 
-  # n <- nrow(condition_combinations)
-  # Apply the logic for subsetting and plotting using data.table
+  # Apply the logic for subsetting and plotting using data.table------------
   condition_combinations[, {
-    # Subset data based on the current combination of conditions
-    # sub_data <- summary_dt_final[
-    #   (divisions[1] == "Light" || Light == .SD$Light) &
-    #     (divisions[1] == "Environment" || Environment == .SD$Environment) &
-    #     (divisions[1] == "Genotype" || Genotype == .SD$Genotype) &
-    #     (divisions[1] == "Treatment" || Treatment == .SD$Treatment) &
-    #     (divisions[1] == "Temperature" || Temperature == .SD$Temperature) &
-    #     (divisions[1] == "Sex" || Sex == .SD$Sex),
-    # ]
-    sub_data <- summary_dt_final[
+    plot_subdata2 <- summary_dt_final[
       Reduce(`&`, lapply(c("Light", "Environment", "Genotype", "Treatment", "Temperature", "Sex"), function(col) {
         if (divisions[1] == col) {
           return(TRUE)  # Skip this condition if divisions[1] matches the column name
-        } else {
-          return(get(col) == .SD[[col]])  # Compare the column if it doesn't match divisions[1]
-        }
-      }))
-    ]
-
+        } else {return(get(col) == .SD[[col]])  # Compare the column if it doesn't match divisions[1]
+          }}))]
     # Curate data for plotting
-    plot_subdata <- dt_curated_final[id %in% sub_data$id]
-    plot_subdata2 <- summary_dt_final[id %in% sub_data$id] ## = subdata
+    plot_subdata <- dt_curated_final[id %in% plot_subdata2$id]
 
     p1title <- trimws(paste(
       if (divisions[1] != "Genotype" && length(unique(condition_combinations$Genotype))>1) {Genotype},
@@ -136,23 +102,15 @@ if (any(plot_data[,Treatment] == "Grp") & any(plot_data[,Treatment ] == "Iso")){
       if (divisions[1] != "Treatment" && length(unique(condition_combinations$Treatment))>1) {Treatment},
       if (divisions[1] != "Temperature" && length(unique(condition_combinations$Temperature))>1) {Temperature},
       if (divisions[1] != "Sex" && length(unique(condition_combinations$Sex))>1) {Sex},
-      if (divisions[1] != "Environment" && length(unique(condition_combinations$Environment))>1) {Environment}
-      #
-      # if (divisions[1] != "Sex" && !is.na(Sex) && Sex != "NA") {Sex},
-      # if (divisions[1] != "Genotype" && !is.na(Genotype) && Genotype != "NA") {Genotype},
-      # if (divisions[1] != "Light" && !is.na(Light) && Light != "NA") {Light},
-      # if (divisions[1] != "Treatment" && !is.na(Treatment) && Treatment != "NA") {Treatment},
-      # if (divisions[1] != "Temperature" && !is.na(Temperature) && Temperature != "NA") {Temperature},
-      # if (divisions[1] != "Environment" && !is.na(Environment) && Environment != "NA") {Environment}
-    ))
+      if (divisions[1] != "Environment" && length(unique(condition_combinations$Environment))>1) {Environment}))
 
-        # Create overlay sleep plot
+        # Create overlay sleep plot--------------
           p1 <- ggetho::ggetho(plot_subdata, ggplot2::aes(y = asleep, colour = .data[[divisions[1]]]), time_wrap = behavr::hours(24)) +
             ggetho::stat_pop_etho(show.legend = T) +
             ggetho::stat_ld_annotations() +
             ggplot2::scale_color_manual(values = c("#0000FF", "#FF0000", "#008B8B", "#808080", "#FFA500","#ADD8E6")) +
             ggplot2::scale_fill_manual(values = c("#0000FF", "#FF0000", "#008B8B", "#808080", "#FFA500","#ADD8E6")) +
-            ggplot2::labs(title = p1title, y= "Sleep (%)") +
+            ggplot2::labs(title = p1title[length(p1title)], y= "Sleep (%)") +
             ggplot2::scale_y_continuous(limits = c(0,1), labels = scales::percent)+
             ggprism::theme_prism(base_fontface = font) +
             ggplot2::theme(title = ggplot2::element_text(size = 22),
@@ -162,25 +120,45 @@ if (any(plot_data[,Treatment] == "Grp") & any(plot_data[,Treatment ] == "Iso")){
                            axis.text.y = ggplot2::element_text(size = 16),
                            legend.text = ggplot2::element_text(size = 16, face = font))
             if(length(unique(plot_subdata2[[divisions[1]]])) <= 2){
-              p1 <- p1 + ggplot2::theme(legend.position.inside = c(0.8,0.15))}
+              p1 <- p1 + ggplot2::theme(legend.position = c(0.8,0.15))}
 
+    #-------------
+    yParams<- c("Sleep_Time_All", "Sleep_Time_L", "Sleep_Time_D", "n_Bouts_L", "n_Bouts_D")
+    if (any(plot_subdata2[,Treatment] == "Grp") & any(plot_subdata2[,Treatment ] == "Iso")){
+      # Perform t-test if the 'Treatment' column has both 'Grp' and 'Iso'
+      p_value <- list()
+
+      for(yParam in yParams){
+        t_test_result <- t.test(plot_subdata2[Treatment == "Grp", get(yParam)],
+                                plot_subdata2[Treatment == "Iso", get(yParam)])
+        p_value[[yParam]] <- t_test_result$p.value
+      }
+      p_values[, get("p1title") := p_value]
+      p_v<- unlist(p_value)
+    } else {p_v <<- NULL}
         # Generate sleep duration plots
-        p2 <- create_sleeptime_plot(plot_subdata2, "Sleep_Time_All", "Total Sleep (min)", 1500, "bar", font)
-        p3 <- create_sleeptime_plot(plot_subdata2, "Sleep_Time_L", "Daytime Sleep (min)", 1000, "bar", font)
-        p4 <- create_sleeptime_plot(plot_subdata2, "Sleep_Time_D", "Nighttime Sleep (min)", 1000, "bar", font)
-        p5 <- create_sleeptime_plot(plot_subdata2, "n_Bouts_L", "Daytime Sleep Bouts", 80, "violin", font)
-        p6 <- create_sleeptime_plot(plot_subdata2, "n_Bouts_D", "Nighttime Sleep Bouts", 80, "violin", font)
-
+        p2 <- create_sleeptime_plot(plot_subdata2, yParams[1], "Total Sleep (min)", 1500, "bar", font, p_v[1])
+        p3 <- create_sleeptime_plot(plot_subdata2, yParams[2], "Daytime Sleep (min)", 1000, "bar", font, p_v[2])
+        p4 <- create_sleeptime_plot(plot_subdata2, yParams[3], "Nighttime Sleep (min)", 1000, "bar", font, p_v[3])
+        p5 <- create_sleeptime_plot(plot_subdata2, yParams[4], "# Daytime Sleep Bouts", 80, "violin", font, p_v[4])
+        p6 <- create_sleeptime_plot(plot_subdata2, yParams[5], "# Nighttime Sleep Bouts", 80, "violin", font, p_v[5])
 
         # Combine plots
-        u <- length(unique(plot_subdata2[[divisions[1]]])) # Dynamic width adjustment
          suppressWarnings(
-          combined_plot <- cowplot::plot_grid(p1, p2, p3, p4, p5, p6, ncol = 6, align = "h", axis = "tb",
+          combined_plot <- cowplot::plot_grid(p1 , p2, p3, p4, p5, p6, ncol = 6, align = "h", axis = "tb",
                                    rel_widths = c(6, u, u, u, u, u))
          )
         p1title <- gsub(" ", "_", p1title)
         # Save combined plot
-        ggplot2::ggsave(paste0("CombinedPlots", p1title, ".pdf"), combined_plot, width = (6 + u * 5 + 1.45), height = 4)
+        ggplot2::ggsave(paste0("CombinedPlots", p1title, ExperimentData@Batch, ".pdf"), combined_plot, width = (6 + u * 5 + 1.45), height = 4)
 
 }, by = 1:nrow(condition_combinations)]
+
+  #-------------
+
+  pvdf <- data.frame(matrix(unlist(p_values), nrow = 5, byrow = TRUE))
+  rownames(pvdf) <- c("Sleep_Time_All", "Sleep_Time_L", "Sleep_Time_D", "n_Bouts_L", "n_Bouts_D")
+  colnames(pvdf)<- names(p_values)
+  write.csv(pvdf, paste0("pValues_", ExperimentData@Batch, ".csv"))
+
 }
