c8e6b7e44479723da7fb79fd0d3422b5196bece0
Edoria <estheridoria@gmail.com>
2025-02-04 12:48:35 -0600

    build package
    put original in fornow

--- R/zzz.R
+++ R/zzz.R
@@ -24,33 +24,3 @@
   }
 
 }
-
-
-# parent_dir <- dirname(rstudioapi::getSourceEditorContext()$path)
-# setwd(parent_dir)
-#
-# source("setStatus.R")
-# source("runOneBatch.R")
-# source("concatenate.R")
-# source("writeLoading.R")
-# source("activityAndSleep.R")
-# source("aliveVsDead.R")
-# source("manualDeadRemoval.R")
-# source("cleanSummary.R")
-# source("genotypePlots.R")
-# source("statsSummary.R")
-# source("normSummary.R")
-# source("generateSE.R")
-# source("summarySE.R")
-# source("concatenate.R")
-# source("corMat.R")
-# source("kmeansCluster.R")
-# source("normDisplay.R")
-# source("plotPreferences.R")
-# source("processDays.R")
-# source("runAllBatches.R")
-# source("concatGenotypePlots.R")
-
-
-
-
